0516498381d4de22bd58d8b528bca436db52d240
اكرم <@DESKTOP-ACEI53V>
2022-10-10 08:49:52 +0300

    add instruction display hello everybody

---
+++ src/monday/Monday.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package monday;
+
+/**
+ *
+ * @author اكرم
+ */
+public class Monday {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello everybody");// TODO code application logic here
+    }
+    
+}
